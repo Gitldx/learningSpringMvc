6edda53e48a017923330a0990b6566983137a395
ldx <ldx@hello.com>
2018-05-17 14:55:42 +0800

    first config

--- src/main/webapp/WEB-INF/views/web/config-overrides.js
+++ src/main/webapp/WEB-INF/views/web/config-overrides.js
@@ -1,9 +1,17 @@
+const path = require('path');
+
 const tsImportPluginFactory = require('ts-import-plugin')
 const { getLoader } = require("react-app-rewired");
 const rewireLess = require('react-app-rewire-less');
 const theme = require("./src/custom.js");
 
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const fs = require('fs');
+const appDirectory = fs.realpathSync(process.cwd());
+const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
 
 module.exports = function override(config, env) {
   const tsLoader = getLoader(
@@ -29,6 +37,59 @@ module.exports = function override(config, env) {
     //modifyVars: { "@primary-color": "#1DA57A","@layout-sider-background":"blue","@menu-dark-color" : "lightblue" },
     modifyVars : theme
   })(config, env);
+
+
+
+  
+ /*  config.entry = {
+    index:[
+      require.resolve('react-dev-utils/webpackHotDevClient'),
+      require.resolve('./config/polyfills'),
+      require.resolve('react-error-overlay'),
+      'src/index.tsx'
+    ],
+    admin:[
+      require.resolve('react-dev-utils/webpackHotDevClient'),
+      require.resolve('./config/polyfills'),
+      require.resolve('react-error-overlay'),
+      "src/admin.tsx"
+    ]
+  }
+  config.output ={
+    pathinfo: true,
+    path: path.join(__dirname, "build"),
+    // This does not produce a real file. It's just the virtual path that is
+    // served by WebpackDevServer in development. This is the JS bundle
+    // containing code from all our entry points, and the Webpack runtime.
+    filename: 'static/js/[name]bundle.js',
+    // There are also additional JS chunk files if you use code splitting.
+    chunkFilename: 'static/js/[name].chunk.js',
+    // This is the URL that app is served from. We use "/" in development.
+    publicPath: '/',
+    // Point sourcemap entries to original disk location (format as URL on Windows)
+    devtoolModuleFilenameTemplate: info =>
+      path.resolve(info.absoluteResourcePath).replace(/\\/g, '/'),
+  }
+
+  config.plugins.push(
+    new HtmlWebpackPlugin({
+      inject: true,
+      chunks: ["index"],
+      template: resolveApp('public/index.html'),
+    }),
+  );
+
+  config.plugins.push(
+    
+    new HtmlWebpackPlugin({
+      inject: true,
+      chunks: ["admin"],
+      template: resolveApp('public/admin.html'),
+      filename: 'admin.html',
+    }),
+  ); */
+
+
 
 
   // config.externals= {
